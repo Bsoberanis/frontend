def0d28b2dcf2e64dc5ca959f5cf43284d3eeaa7
Bsoberanis <bsoberanis-2023433@kinal.edu.gt>
2025-04-02 11:52:30 -0600

    hola chabales este es el comienso del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import router from "./routes";
+
+export const App = () =>{
+    
+    let element = useRouter(router);
+    return ( 
+        <>
+        {element}
+        <Toaster
+        position="botton-ringht"
+        reverseOrder={false}
+        
+        />
+        </>
+        
+        
+    )
+}
